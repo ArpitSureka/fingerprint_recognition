d130c0a38e0d632bfe04aa166b79ae4287f33dbc
Arpit Sureka <surekaarpit@gmail.com>
2024-04-13 06:56:54 +0000

    changes

--- finegerprint_pipline.py
+++ finegerprint_pipline.py
@@ -44,34 +44,39 @@ def fingerprint_pipline(input_img):
     thin_image = skeletonize(gabor_img)
 
     # minutias
-    minutias = calculate_minutiaes(thin_image)
+    # minutias = calculate_minutiaes(thin_image)
 
     # singularities
-    singularities_img = calculate_singularities(thin_image, angles, 1, block_size, mask)
+    # singularities_img = calculate_singularities(thin_image, angles, 1, block_size, mask)
 
     # visualize pipeline stage by stage
-    output_imgs = [input_img, normalized_img, segmented_img, orientation_img, gabor_img, thin_image, minutias, singularities_img]
-    for i in range(len(output_imgs)):
-        if len(output_imgs[i].shape) == 2:
-            output_imgs[i] = cv.cvtColor(output_imgs[i], cv.COLOR_GRAY2RGB)
-    results = np.concatenate([np.concatenate(output_imgs[:4], 1), np.concatenate(output_imgs[4:], 1)]).astype(np.uint8)
+    # output_imgs = [input_img, normalized_img, segmented_img, orientation_img, gabor_img, thin_image, minutias, singularities_img]
+    # for i in range(len(output_imgs)):
+    #     if len(output_imgs[i].shape) == 2:
+    #         output_imgs[i] = cv.cvtColor(output_imgs[i], cv.COLOR_GRAY2RGB)
+    # results = np.concatenate([np.concatenate(output_imgs[:4], 1), np.concatenate(output_imgs[4:], 1)]).astype(np.uint8)
 
-    return results
+    return gabor_img
 
 
 if __name__ == '__main__':
     # open images
-    img_dir = './sample_inputs/*'
-    output_dir = './output/'
+    img_dir = './sample_inputs2/*'
+    output_dir = './output2/'
+    os.makedirs(output_dir, exist_ok=True)
+
     def open_images(directory):
         images_paths = glob(directory)
-        return np.array([cv.imread(img_path,0) for img_path in images_paths])
+        for i, img_path in enumerate(images_paths):
+            results = fingerprint_pipline(np.array(cv.imread(img_path,0)))
+            cv.imwrite(output_dir+str(i)+'.png', results)
+        # return np.array([cv.imread(img_path,0) for img_path in images_paths])
 
     images = open_images(img_dir)
 
     # image pipeline
-    os.makedirs(output_dir, exist_ok=True)
-    for i, img in enumerate(tqdm(images)):
-        results = fingerprint_pipline(img)
-        cv.imwrite(output_dir+str(i)+'.png', results)
+    
+    # for i, img in enumerate(tqdm(images)):
+    #     results = fingerprint_pipline(img)
+    #     cv.imwrite(output_dir+str(i)+'.png', results)
         # cv.imshow('image pipeline', results); cv.waitKeyEx()
